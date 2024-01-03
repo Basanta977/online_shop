7f39a1ff18628f42dfaebbe2c1cbb6fbd9ad4dfa
Basanta977 <107608674+Basanta977@users.noreply.github.com>
2024-01-03 16:25:10 +0545

    setup initial project

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $(".one").owlCarousel({
+        items:1, 
+        nav:true,
+        dots:false
+    });
+    $(".two").owlCarousel({
+        items:4, 
+        nav:true,
+        dots:false,
+        margin: 20,
+    });
+  });
